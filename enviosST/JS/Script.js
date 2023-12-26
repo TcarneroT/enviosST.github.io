51c77444799295c29f2b9b9d6b0804f7bd40f0aa
Andrés Felipe Cárdenas Escobar <119759954+TcarneroT@users.noreply.github.com>
2023-12-26 00:22:22 -0500

    initial commit

---
+++ enviosST/JS/Script.js
@@ -0,0 +1,25 @@
+const targets = document.querySelectorAll('[data-target]')
+const content = document.querySelectorAll('[data-content]')
+
+
+targets.forEach(target => {
+
+    target.addEventListener('click', ()=>{
+
+        content.forEach(c => {
+
+            c.classList.remove('active')
+
+        })
+
+
+        const t=document.querySelector(target.dataset.target)
+        t.classList.add('active')
+    })
+
+})
+
+function ocultar(){
+document.getElementById('textBienvenidad').style.display= 'none';
+
+}
